c4315488982b311057c212a4c56a50a9b7783130
SkulicAmir <34455809+SkulicAmir@users.noreply.github.com>
2018-02-21 12:07:25 -0600

    Add files via upload

---
+++ maps.py
@@ -0,0 +1,9 @@
+from tkinter import *
+
+class Map:
+
+    def __init__(self, window, bg="black", width=600, height=400):
+        self.width = width
+        self.height = height
+        self.bg = bg
+
